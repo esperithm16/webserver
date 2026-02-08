b428dc904f761a91d2fa2e8a640ff1ec1b34192f
esperithm16 <rajatshaily17@gmail.com>
2026-02-08 23:53:32 +0530

    last for the day

--- ThreadPool/Server.java
+++ ThreadPool/Server.java
@@ -1,9 +1,25 @@
 package ThreadPool;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Server {
+    private final ExecutorService threadPool;
+    public Server(int pooolSize){
+        this.threadPool= Executors.newFixedThreadPool(pooolSize);
+    }
+    public void handleClient(Socket clientSocket){
+        try {PrintWriter toSocket=new PrintWriter(clientSocket.getOutputStream(),true)
+            {toSocket.println("hello from the server"+clientSocket.getInetAddress());}
+            
+        } catch (IOException ex) {
+         ex.printStackTrace();   // TODO: handle exception
+        }
+    }
    public static void main(String[] args) {
     int port=8010;
     int pooolSize=10;
@@ -17,7 +33,9 @@ public class Server {
           server.ThreadPool.execute(()->server.handleClient(clientSocket));
         }
     } catch (IOException ex) {
-        // TODO: handle exception
+      ex.printStackTrace();  // TODO: handle exception
+    } finally{
+        server.ThreadPool.shutdown();
     }
    } 
 }

26a6826e595dbbec20d52fa19803c1fb24c2c50f
esperithm16 <rajatshaily17@gmail.com>
2026-02-08 23:42:51 +0530

    threadpool server

--- ThreadPool/Server.java
+++ ThreadPool/Server.java
@@ -1,5 +1,23 @@
 package ThreadPool;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
-    
+   public static void main(String[] args) {
+    int port=8010;
+    int pooolSize=10;
+    Server server = new Server(pooolSize);
+    try {
+        ServerSocket serverSocket=new ServerSocket(port);
+        serverSocket.setSoTimeout(70000);
+        System.out.println("Server is listening on port"+port);
+        while (true) {
+          Socket clientSocket  = serverSocket.accept();
+          server.ThreadPool.execute(()->server.handleClient(clientSocket));
+        }
+    } catch (IOException ex) {
+        // TODO: handle exception
+    }
+   } 
 }

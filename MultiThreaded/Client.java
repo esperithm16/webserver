2259c8ea1e172a6c4ea6e81060e7b7a0b195fdd6
esperithm16 <rajatshaily17@gmail.com>
2026-02-08 21:20:22 +0530

    hiiii

--- MultiThreaded/Client.java
+++ MultiThreaded/Client.java
@@ -1,4 +1,12 @@
 public class Client{
+    public Runnable getRunnable(){
+        return new Runnable() {
+            @Override
+            public void run(){
+                int port = 8010;
+            }
+        };
+    }
     public static void main(String[] args) {
     Client client = new Client();
     for(int i = 0 ; i<100;i++){
